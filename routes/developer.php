e5d8ff77de212eeee57328cc3cfec0272798bc3e
BD <bhavdip.inexture@gmail.com>
2017-11-02 09:56:54 +0530

    Data Point

--- routes/developer.php
+++ routes/developer.php
@@ -1,12 +1,6 @@
 <?php
 
 Route::get('/home', function () {
-    $users[] = Auth::user();
-    $users[] = Auth::guard()->user();
-    $users[] = Auth::guard('developer')->user();
-
-    //dd($users);
-
     return view('developer.home');
 })->name('home');
 
