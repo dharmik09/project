e14b83f2e41e05846c8c08660dca1ec0029f6a5b
BD <bhavdip.inexture@gmail.com>
2017-12-18 15:29:26 +0530

    Set Data for profile

--- app/Http/Middleware/RedirectIfNotTeenager.php
+++ app/Http/Middleware/RedirectIfNotTeenager.php
@@ -4,9 +4,24 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Contracts\Auth\Guard;
+use Redirect;
+use Config;
+use DB;
+use Route;
+use Request;
+use App\CMS;
+use App\Services\Teenagers\Contracts\TeenagersRepository;
+
 
 class RedirectIfNotTeenager
 {
+	public function __construct(Guard $auth, TeenagersRepository $teenagersRepository)
+    {
+        $this->auth = $auth;
+        $this->cmsObj = new CMS();
+        $this->teenagersRepository = $teenagersRepository;
+    }
 	/**
 	 * Handle an incoming request.
 	 *
@@ -17,10 +32,32 @@ class RedirectIfNotTeenager
 	 */
 	public function handle($request, Closure $next, $guard = 'teenager')
 	{
-	    if (!Auth::guard($guard)->check()) {
+		if (!Auth::guard($guard)->check()) {
 	        return redirect('teenager/login');
 	    }
 
+	    $teenagerId = Auth::guard('teenager')->id();
+	    $teenager = $this->teenagersRepository->saveTeenagerActivityDetail($teenagerId);
+        $notDeleted = DB::table(config::get('databaseconstants.TBL_TEENAGERS'))->where('id', $teenagerId)->where('deleted', '1')->first();
+
+        if(isset($notDeleted) && !empty($notDeleted)) {
+            if($notDeleted->t_isverified == 0) {
+                Auth::guard('teenager')->logout();
+                return Redirect::to('teenager/login')->with('error', 'Your account has not verified, Please contact administrator for the same.');
+                exit;
+            }
+
+            if(($notDeleted->t_name == '' || $notDeleted->t_lastname == '' || $notDeleted->t_pincode == '' || $notDeleted->t_email == '' || $notDeleted->t_gender == '' || $notDeleted->t_gender == 0 || $notDeleted->t_birthdate == '') && Request::path() != 'teenager/edit-profile' && Request::path() != 'teenager/my-profile' && Request::path() != 'teenager/save-profile'){
+                return Redirect::to('teenager/edit-profile')->with('error', 'Please fillup the all mandatory fields to use the application.');
+                exit;
+            }
+
+        } else {
+            Auth::guard('teenager')->logout();
+            return Redirect::to('teenager/login')->with('error', 'Your account either deleted or inactive.');
+            exit;
+        }    
+	    
 	    return $next($request);
 	}
 }
