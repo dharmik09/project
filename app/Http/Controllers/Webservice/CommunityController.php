f46c06444df64aab3bedb51966c157b6be634dde
Mansi <mansi.kanani@inexture.in>
2018-01-08 15:40:52 +0530

    Add community controller
    Signed-off-by: Mansi <mansi.kanani@inexture.in>

---
+++ app/Http/Controllers/Webservice/CommunityController.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Http\Controllers\Webservice;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
+use Illuminate\Http\Request;
+use App\Services\Teenagers\Contracts\TeenagersRepository;
+use App\Services\Community\Contracts\CommunityRepository;
+use Config;
+use Storage;
+use Helpers;
+
+class CommunityController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(TeenagersRepository $teenagersRepository, CommunityRepository $communityRepository)
+    {
+        //$this->middleware('admin.guest', ['except' => 'logout']);
+        $this->communityRepository = $communityRepository;
+        $this->teenagersRepository = $teenagersRepository;
+        $this->teenagerThumbImageUploadPath = Config::get('constant.TEEN_THUMB_IMAGE_UPLOAD_PATH');
+    }
+
+    /* Request Params : communityNewConnections
+     *  loginToken, userId
+     *  Service after loggedIn user
+     */
+    public function communityNewConnections(Request $request)
+    {
+        $response = [ 'status' => 0, 'login' => 0, 'message' => trans('appmessages.default_error_msg') ] ;
+        $teenager = $this->teenagersRepository->getTeenagerById($request->userId);
+        if($request->userId != "" && $teenager) {
+            $newConnections = $this->communityRepository->getNewConnections($request->userId);
+            $data = [];
+            $data['sortBy'] = Helpers::getCommunitySortByArray();
+            $data['newConnections'] = [];
+            if(isset($newConnections) && !empty($newConnections) && count($newConnections) > 0) {
+                foreach($newConnections as $newConnection) {
+                    $newConnection->t_photo  = ($newConnection->t_photo != "") ? Storage::url($this->teenagerThumbImageUploadPath.$newConnection->t_photo) : Storage::url($this->teenagerThumbImageUploadPath."proteen-logo.png");
+                    $data['newConnections'][] = $newConnection;
+                }
+            }
+            $response['login'] = 1;
+            $response['status'] = 1;
+            $response['message'] = trans('appmessages.default_success_msg');
+            $response['data'] = $data;
+
+        } else {
+            $response['message'] = trans('appmessages.invalid_userid_msg') . ' or ' . trans('appmessages.notvarified_user_msg');
+        }
+        return response()->json($response, 200);
+        exit;
+    }
+
+    /* Request Params : communityMyConnections
+     *  loginToken, userId
+     *  Service after loggedIn user
+     */
+    public function communityMyConnections(Request $request)
+    {
+        $response = [ 'status' => 0, 'login' => 0, 'message' => trans('appmessages.default_error_msg') ] ;
+        $teenager = $this->teenagersRepository->getTeenagerById($request->userId);
+        if($request->userId != "" && $teenager) {
+            $myConnections = $this->communityRepository->getMyConnections($request->userId);
+            $data = [];
+            $data['sortBy'] = Helpers::getCommunitySortByArray();
+            $data['myConnections'] = [];
+            if(isset($myConnections) && !empty($myConnections) && count($myConnections) > 0) {
+                foreach($myConnections as $myConnection) {
+                    $myConnection->t_photo  = ($myConnection->t_photo != "") ? Storage::url($this->teenagerThumbImageUploadPath.$myConnection->t_photo) : Storage::url($this->teenagerThumbImageUploadPath."proteen-logo.png");
+                    $data['myConnections'][] = $myConnection;
+                }
+            }
+            $response['login'] = 1;
+            $response['status'] = 1;
+            $response['message'] = trans('appmessages.default_success_msg');
+            $response['data'] = $data;
+
+        } else {
+            $response['message'] = trans('appmessages.invalid_userid_msg') . ' or ' . trans('appmessages.notvarified_user_msg');
+        }
+        return response()->json($response, 200);
+        exit;
+    }
+
+    /* Request Params : searchCommunityMyConnections
+     *  loginToken, userId
+     *  Service after loggedIn user
+     */
+    public function searchCommunityMyConnections(Request $request)
+    {
+        $response = [ 'status' => 0, 'login' => 0, 'message' => trans('appmessages.default_error_msg') ] ;
+        $teenager = $this->teenagersRepository->getTeenagerById($request->userId);
+        if($request->userId != "" && $teenager) {
+            if($request->searchText != "") {
+                $myConnections = $this->communityRepository->getMyConnections($request->userId, $request->searchText);
+            } else {
+                $myConnections = $this->communityRepository->getMyConnections($request->userId);
+            }
+            $data = [];
+            $data['sortBy'] = Helpers::getCommunitySortByArray();
+            $data['myConnections'] = [];
+            if(isset($myConnections) && !empty($myConnections) && count($myConnections) > 0) {
+                foreach($myConnections as $myConnection) {
+                    $myConnection->t_photo  = ($myConnection->t_photo != "") ? Storage::url($this->teenagerThumbImageUploadPath.$myConnection->t_photo) : Storage::url($this->teenagerThumbImageUploadPath."proteen-logo.png");
+                    $data['myConnections'][] = $myConnection;
+                }
+            }
+            $response['login'] = 1;
+            $response['status'] = 1;
+            $response['message'] = trans('appmessages.default_success_msg');
+            $response['data'] = $data;
+
+        } else {
+            $response['message'] = trans('appmessages.invalid_userid_msg') . ' or ' . trans('appmessages.notvarified_user_msg');
+        }
+        return response()->json($response, 200);
+        exit;
+    }
+
+    /* Request Params : searchCommunityNewConnections
+     *  loginToken, userId
+     *  Service after loggedIn user
+     */
+    public function searchCommunityNewConnections(Request $request)
+    {
+        $response = [ 'status' => 0, 'login' => 0, 'message' => trans('appmessages.default_error_msg') ] ;
+        $teenager = $this->teenagersRepository->getTeenagerById($request->userId);
+        if($request->userId != "" && $teenager) {
+            if($request->searchText != "") {
+                $newConnections = $this->communityRepository->getNewConnections($request->userId, $request->searchText);
+            } else {
+                $newConnections = $this->communityRepository->getNewConnections($request->userId, $request->searchText);
+            }
+            $data = [];
+            $data['sortBy'] = Helpers::getCommunitySortByArray();
+            $data['newConnections'] = [];
+            if(isset($newConnections) && !empty($newConnections) && count($newConnections) > 0) {
+                foreach($newConnections as $newConnection) {
+                    $newConnection->t_photo  = ($newConnection->t_photo != "") ? Storage::url($this->teenagerThumbImageUploadPath.$newConnection->t_photo) : Storage::url($this->teenagerThumbImageUploadPath."proteen-logo.png");
+                    $data['newConnections'][] = $newConnection;
+                }
+            }
+            $response['login'] = 1;
+            $response['status'] = 1;
+            $response['message'] = trans('appmessages.default_success_msg');
+            $response['data'] = $data;
+
+        } else {
+            $response['message'] = trans('appmessages.invalid_userid_msg') . ' or ' . trans('appmessages.notvarified_user_msg');
+        }
+        return response()->json($response, 200);
+        exit;
+    }
+}
