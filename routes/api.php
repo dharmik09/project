aa17b958fa83a24edf02aea2f18e9265fdf952c2
BD <bhavdip.inexture@gmail.com>
2018-01-10 18:41:45 +0530

    Change level data

--- routes/api.php
+++ routes/api.php
@@ -33,11 +33,12 @@ Route::group([ 'middleware' => ['api-outside'] ], function () {
 Route::group([ 'middleware' => ['api-support'] ], function () {
 	Route::post('/setPassword', 'Webservice\PasswordController@setPassword');
 	Route::post('/changePassword', 'Webservice\PasswordController@changePassword');
-	
+	//profile page
 	Route::post('/updateProfile', 'Webservice\ProfileController@updateProfile');
 	Route::post('/getTeenagerProfileData', 'Webservice\ProfileController@getTeenagerProfileData');
 	Route::post('/deleteTeenagerData', 'Webservice\ProfileController@deleteTeenagerData');
 	Route::post('/saveTeenagerAboutInfo', 'Webservice\ProfileController@saveTeenagerAboutInfo');
+	Route::post('/getTeenagerEarnAchievement', 'Webservice\ProfileController@getTeenagerEarnAchievement');
 
 	Route::post('/getActiveTeenages', 'Webservice\TeenagerController@getActiveTeenages');
 	Route::post('/getActiveTeenagesBySearch', 'Webservice\TeenagerController@getActiveTeenagesBySearch');
