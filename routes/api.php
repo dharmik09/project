cc4fccf6f264f1d13343e50ea37f6489a7e509fe
BD <bhavdip.inexture@gmail.com>
2018-01-11 14:40:48 +0530

    getLevel1Part2Options

--- routes/api.php
+++ routes/api.php
@@ -48,6 +48,9 @@ Route::group([ 'middleware' => ['api-support'] ], function () {
 	//First Level Part 1 Question/Answer Route
 	Route::post('/getLevel1Questions', 'Webservice\Level1ActivityController@getFirstLevelActivity');
 	Route::post('/submitLevel1Answers', 'Webservice\Level1ActivityController@saveFirstLevelActivity');
+	//First Level Part 2 Route
+	Route::post('/getLevel1Part2Options', 'Webservice\Level1ActivityController@getLevel1Part2Options');
+	Route::post('/submitLevel1Answers', 'Webservice\Level1ActivityController@saveFirstLevelActivity');
 	//Education & Achievement Route
 	Route::post('/getTeenagerAcademicInfo', 'Webservice\ProfileController@getTeenagerAcademicInfo');
 	Route::post('/saveTeenagerAcademicInfo', 'Webservice\ProfileController@saveTeenagerAcademicInfo');
