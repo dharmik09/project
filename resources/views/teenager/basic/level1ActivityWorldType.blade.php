3f7d8f9f0276c93045cbc8935644c9276cc6f072
BD <bhavdip.inexture@gmail.com>
2018-01-19 09:20:48 +0530

    Data file

--- resources/views/teenager/basic/level1ActivityWorldType.blade.php
+++ resources/views/teenager/basic/level1ActivityWorldType.blade.php
@@ -1,57 +1,40 @@
-<div class="inner_container">
-    <div class="landing_container">
-        <div>
-            <div class="col-md-6 col-sm-6 col-xs-6 borderright">
-                <a href="<?php if($isQuestionCompleted == 0) { ?> {{ url('/teenager/playLevel1Activity') }} <?php } else { echo "javascript:void(0)"; }?>" class="landing_box landing_l1" onclick="checkLevel1Questions({{$isQuestionCompleted}});">
-                    <span class="img_container">
-                        <span class="landing_icon">
-                            <span class="vote"></span>
-                        </span>
-                    </span>
-                    <span class="title_container">
-                        <span class="main_title">Opinions</span>
-                    </span>
+<div class="level-selection">
+    <h3>Vote</h3>
+    <div class="level-container">
+        <div class="row">
+            <div class="col-xs-6 border-right">
+                <a href="javascript:void(0)" class="level-block" title="Opinions">
+                    <a href="<?php if($isQuestionCompleted == 0) { ?> {{ url('/teenager/playLevel1Activity') }} <?php } else { echo "javascript:void(0)"; }?>" class="landing_box landing_l1" onclick="checkLevel1Questions({{$isQuestionCompleted}});">
+                    <figure class="landing_icon">
+                        <span class="vote"></span>
+                    </figure>
+                        <span>Opinions</span>
                 </a>
             </div>
-            <div class="col-md-6 col-sm-6 col-xs-6 borderbottom">
-                <a href="javascript:void(0);" onClick="playFirstLevelWorldType(1)" class="landing_box landing_l2">
-                    <span class="img_container">
-                        <span class="landing_icon">
-                            <span class="fiction"></span>
-                        </span>
-                    </span>
-                    <span class="title_container">
-                        <span class="main_title">Fictional World</span>
-                    </span>
+            <div class="col-xs-6 border-bottom">
+                <a href="javascript:void(0)" onClick="playFirstLevelWorldType(1)" class="level-block" title="Fictional World">
+                    <figure class="landing_icon">
+                        <span class="fiction"></span>
+                    </figure>
+                        <span>Fictional World</span>
                 </a>
             </div>
-            <br/>
-            <div class="row">
-                <div class="col-md-6 col-sm-6 col-xs-6 bordertop">
-                    <a onClick="playFirstLevelWorldType(2)" href="javascript:void(0);" class="landing_box landing_l3 right">
-                        <span class="img_container">
-                            <span class="landing_icon">
-                                <span class="real"></span>
-                            </span>
-                        </span>
-                        <span class="title_container">
-                            <span class="main_title">Real World</span>
-                        </span>
-                    </a>
-                </div>
-                <div class="col-md-6 col-sm-6 col-xs-6 borderleft">
-                    <a onClick="playFirstLevelWorldType(3)" href="javascript:void(0);" class="landing_box landing_l4">
-                        <span class="img_container">
-                            <span class="landing_icon">
-                                <span class="family"></span>
-                            </span>
-                        </span>
-                        <span class="title_container">
-                            <span class="main_title">Your World</span>
-                        </span>
-                    </a>
-                </div>
+            <div class="col-xs-6 border-top">
+                <a href="javascript:void(0)" onClick="playFirstLevelWorldType(2)" class="level-block" title="Real World">
+                    <figure class="landing_icon">
+                        <span class="real"></span>
+                    </figure>
+                        <span>Real World</span>
+                </a>
+            </div>
+            <div class="col-xs-6 border-left">
+                <a href="javascript:void(0)" onClick="playFirstLevelWorldType(3)" class="level-block" title="Your World">
+                    <figure class="landing_icon">
+                        <span class="family"></span>
+                    </figure>
+                    <span>Your World</span>
+                </a>
             </div>
         </div>
     </div>
-</div>
+</div>            
