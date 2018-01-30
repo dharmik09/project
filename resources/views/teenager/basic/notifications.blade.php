b21d7106abbbad6bf09a78bf4faf922af3325fa4
Jaimin Pilojpara <jaimin.inexture@gmail.com>
2018-01-30 17:59:10 +0530

    Notification new Table and old table drop Query added

---
+++ resources/views/teenager/basic/notifications.blade.php
@@ -0,0 +1,19 @@
+@foreach($notificationData as $key => $value)
+<div class="notification-block <?php echo ($value->n_read_status == 1) ? 'read' : 'unread' ?>" id="{{$value->id}}notification-block">
+    <div class="notification-img"><img src="{{Storage::url(Config::get('constant.TEEN_ORIGINAL_IMAGE_UPLOAD_PATH').$value->senderTeenager->t_photo)}}" alt="notification img"></div>
+    <div class="notification-content"><a href="#">{!!$value->n_notification_text!!}</a><span class="date">{{Carbon\Carbon::createFromFormat('Y-m-d H:i:s',$value->created_at)->diffForHumans()}}</span>
+        <ul class="btn-list">
+            @if($value->community->tc_status == 1)
+                <li><a href="#" title="accept" class="accept">Accepted</a></li>
+            @elseif($value->community->tc_statsus == 2)
+                <li><a href="#" title="decline" class="decline">Declined</a></li>
+            @elseif($value->community->tc_status == 0)
+                <li><a href="{{url('teenager/accept-request').'/'.$value->n_record_id}}" title="accept" class="accept">Accept</a></li>
+                <li><a href="{{url('teenager/decline-request').'/'.$value->n_record_id}}" title="decline" class="decline">Decline</a></li>
+            @endif
+            <div id="pageWiseNotifications"></div>
+        </ul>
+    </div>
+    <div class="close"><i class="icon-close" onclick="removeNotificationBlock({{$value->id}});"></i></div>
+</div>
+@endforeach
