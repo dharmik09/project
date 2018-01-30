2d00182f6f1565c106a4ba1c1b94a4c5fdb76607
BD <bhavdip.inexture@gmail.com>
2018-01-30 13:08:49 +0530

    Quiz logic

--- app/Jobs/SetProfessionMatchScale.php
+++ app/Jobs/SetProfessionMatchScale.php
@@ -54,6 +54,8 @@ class SetProfessionMatchScale implements ShouldQueue
         if (isset($getCareerMappingFromSystem[0]) && !empty($getCareerMappingFromSystem[0])) { 
             $professionScale = [];
             foreach ($getCareerMappingFromSystem as $keyId => $valueProfession) { 
+                $arrayCombinePoint = [];
+
                 $valueProfession->tcm_scientific_reasoning = (isset($valueProfession->tcm_scientific_reasoning) && $valueProfession->tcm_scientific_reasoning != '') ? $valueProfession->tcm_scientific_reasoning : 'L';
                 $valueProfession->tcm_verbal_reasoning = (isset($valueProfession->tcm_verbal_reasoning) && $valueProfession->tcm_verbal_reasoning != '') ? $valueProfession->tcm_verbal_reasoning : 'L';
                 $valueProfession->tcm_numerical_ability = (isset($valueProfession->tcm_numerical_ability) && $valueProfession->tcm_numerical_ability != '') ? $valueProfession->tcm_numerical_ability : 'L';
@@ -149,7 +151,6 @@ class SetProfessionMatchScale implements ShouldQueue
                     $professionScale[$valueProfession->tcm_profession] = $answer['matchScale'];
                 }
             }
-            //echo "<pre/>"; print_r($professionScale); die();
             if(count($professionScale) > 0) {
                 $jsonData = json_encode($professionScale);
                 $array = [];
