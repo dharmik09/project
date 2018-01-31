506e5fd4790319414804608d6e1e0b9a9e7bdc59
inxronak <ronak.luhar@inexture.in>
2018-01-31 14:57:18 +0530

    setup profile issue

--- app/Http/Middleware/RedirectIfNotTeenager.php
+++ app/Http/Middleware/RedirectIfNotTeenager.php
@@ -47,7 +47,7 @@ class RedirectIfNotTeenager
                 exit;
             }
 
-            if(($notDeleted->t_name == '' || $notDeleted->t_lastname == '' || $notDeleted->t_pincode == '' || $notDeleted->t_email == '' || $notDeleted->t_gender == '' || $notDeleted->t_gender == 0 || $notDeleted->t_birthdate == '') && Request::path() != 'teenager/set-profile' && Request::path() != 'teenager/my-profile' && Request::path() != 'teenager/save-profile' && Request::path() != 'teenager/get-phone-code-by-country-for-profile'){
+            if(($notDeleted->t_name == '' || $notDeleted->t_lastname == '' || $notDeleted->t_pincode == '' || $notDeleted->t_email == '' || $notDeleted->t_gender == '' || $notDeleted->t_gender == 0 || $notDeleted->t_birthdate == '') && Request::path() != 'teenager/set-profile' && Request::path() != 'teenager/my-profile' && Request::path() != 'teenager/my-profile#profile-info' && Request::path() != 'teenager/save-profile' && Request::path() != 'teenager/get-phone-code-by-country-for-profile'){
                 return Redirect::to('teenager/set-profile')->with('error', 'Please fillup the all mandatory fields to use the application.');
                 exit;
             }
