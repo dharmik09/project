e0ffa2c3a79e43bd870080076a155f66ba865ce7
Mansi <mansi.kanani@inexture.in>
2018-02-01 17:27:19 +0530

    Listing guru section on Interest Details page
    Signed-off-by: Mansi <mansi.kanani@inexture.in>

--- routes/teenager.php
+++ routes/teenager.php
@@ -18,6 +18,7 @@ Route::get('/gift-coupons', function() {
 });
 Route::get('/interest/{slug}', 'Teenager\InterestManagementController@index');
 Route::post('/see-more-interest-related-careers', 'Teenager\InterestManagementController@seeMoreRelatedCareers');
+Route::post('/see-more-inerest-page-gurus', 'Teenager\InterestManagementController@seeMoreGurus');
 Route::get('/multi-intelligence/{type}/{slug}', 'Teenager\MultipleIntelligenceManagementController@index');
 Route::post('/see-more-related-careers', 'Teenager\MultipleIntelligenceManagementController@seeMoreRelatedCareers');
 Route::post('/see-more-gurus', 'Teenager\MultipleIntelligenceManagementController@seeMoreGurus');
