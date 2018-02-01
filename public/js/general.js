a228469ddf1d00172bcd99063498c73c14863216
BD <bhavdip.inexture@gmail.com>
2018-02-01 16:03:21 +0530

    Data

--- public/js/general.js
+++ public/js/general.js
@@ -356,9 +356,9 @@ $(document).keydown(function (e) {
     }
 });
 //loader
-$(window).load(function() {
-		// Animate loader off screen
-		//$("#loading-wrapper").show();
-        $('#loading-wrapper').hide();
-         $('body').removeClass('overflw-hide');
-	});
+$(window).on('load', function(){
+  // Animate loader off screen
+  //$("#loading-wrapper").show();
+    $('#loading-wrapper').hide();
+    $('body').removeClass('overflw-hide');
+ });
