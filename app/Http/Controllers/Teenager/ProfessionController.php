f047cfb674ec82e87285be24fca036391e05d168
BD <bhavdip.inexture@gmail.com>
2018-02-01 13:07:23 +0530

    app data

--- app/Http/Controllers/Teenager/ProfessionController.php
+++ app/Http/Controllers/Teenager/ProfessionController.php
@@ -70,44 +70,7 @@ class ProfessionController extends Controller {
                 $professionAttemptedCount++;
             }
         }
-
-        $return = '<div class="panel-body">
-                    <div class="banner-landing banner-career" style="background-image:url('.Storage::url(Config::get('constant.BASKET_ORIGINAL_IMAGE_UPLOAD_PATH').$basketsData->b_logo).');">
-                        <div class="">
-                            <div class="play-icon"><a id="link'.$basketsData->id.'" onclick="playVideo(this.id,\''.Helpers::youtube_id_from_url($basketsData->b_video).'\');" class="play-btn" id="iframe-video"><img src="'.Storage::url('img/play-icon.png').'" alt="play icon"></a></div>
-                        </div><iframe width="100%" height="100%" frameborder="0" allowfullscreen class="iframe" id="iframe-video-link'.$basketsData->id.'"></iframe>
-                    </div>
-                        <div class="related-careers careers-tag">
-                            <div class="career-heading clearfix">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <p>You have completed <strong>'.$professionAttemptedCount.' of '.count($basketsData->profession).'</strong> careers</p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="pull-right">
-                                            <ul class="match-list">
-                                                <li><span class="number match-strong">4</span> Strong match</li>
-                                                <li><span class="number match-potential">5</span> Potential match</li>
-                                                <li><span class="number match-unlikely">4</span> Unlikely match</li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <ul class="career-list">';
-
-        foreach($basketsData->profession as $k => $v){
-
-            $return .= '<li class="match-strong complete-feild"><a href="'.url('teenager/career-detail/'.$v->pf_slug).'" title="'.$v->pf_name.'">'.$v->pf_name.'</a>';
-                if(isset($v->attempted)){
-                    $return .= '<a class="complete"><span>Complete <i class="icon-thumb"></i></span></a>';
-                }
-            $return .= '</li>';
-        }
-
-        $return .= '</ul></div></div>';
-        
-        return $return;
+        return view('teenager.basic.basketProfession', compact('basketsData', 'professionAttemptedCount'));
     }
 
     public function gridGetIndex(){
