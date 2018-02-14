1ee7828dcef247e105ba0abfba5fb81a34e4d91a
Mansi <mansi.kanani@inexture.in>
2018-02-14 14:35:45 +0530

    CHALLENGE PLAY section of careerDetail page
    Signed-off-by: Mansi <mansi.kanani@inexture.in>

--- app/TeenParentChallenge.php
+++ app/TeenParentChallenge.php
@@ -51,4 +51,19 @@ class TeenParentChallenge  extends Model {
             return false;
         }
     }
+
+    //Get list for challenged parent and mentor
+    public function getChallengedParentAndMentorList($professionId, $teenId) {
+        $result = DB::table(config::get('databaseconstants.TBL_TEENAGER_PARENT_CHALLENGE'). " AS parent_challenge")
+                        ->leftjoin(config::get('databaseconstants.TBL_PROFESSIONS') . " AS profession", 'profession.id', '=', 'parent_challenge.tpc_profession_id')
+                        ->leftjoin(config::get('databaseconstants.TBL_PARENTS') . " AS parent", 'parent.id', '=', 'parent_challenge.tpc_parent_id')
+                        ->selectRaw('parent_challenge.*, profession.pf_name, parent.p_first_name, parent.p_photo, profession.pf_logo')
+                        ->where('parent_challenge.tpc_teenager_id', $teenId)
+                        ->where('parent_challenge.tpc_profession_id', $professionId)
+                        ->where('parent_challenge.deleted','=', 1)
+                        ->where('parent.deleted','=', 1)
+                        ->where('profession.deleted','=', 1)
+                        ->get();
+        return $result;
+    }
 }
