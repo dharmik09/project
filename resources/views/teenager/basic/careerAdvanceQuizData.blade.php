82d5d46372bc3e9273368732d0f62d892fd32a49
Mansi <mansi.kanani@inexture.in>
2018-02-19 16:31:38 +0530

    L4 advance activity - retrieve data for image, doc and video alert
    Signed-off-by: Mansi <mansi.kanani@inexture.in>

---
+++ resources/views/teenager/basic/careerAdvanceQuizData.blade.php
@@ -0,0 +1,34 @@
+@if(isset($activityData) && !empty($activityData))
+<div class="upload-screen quiz-box modal-screen sec-show">
+    <em class="close"><i class="icon-close"></i></em>
+    <?php
+        if (strpos($activityData[0]->l4aa_description, '[PROFESSION_NAME]') !== false) {
+            $professionName = (isset($professionDetail) && !empty($professionDetail)) ? $professionDetail[0]->pf_name : '';
+            $descText = str_replace('[PROFESSION_NAME]', $professionName, $activityData[0]->l4aa_description);
+        } else {
+            $descText = $activityData[0]->l4aa_description;
+        } ?>
+    <h3>{{$descText}}</h3>
+    <h4>Interview Q's</h4>
+    <?php $k = 1; ?>
+    <ol>
+        @foreach($activityData as $key=>$data)
+        <?php
+        if (strpos($data->l4aa_text, '[PROFESSION_NAME]') !== false) {
+            $professionName = (isset($professionDetail) && !empty($professionDetail)) ? $professionDetail[0]->pf_name : '';
+            $questionText = str_replace('[PROFESSION_NAME]', $professionName, $data->l4aa_text);
+        } else {
+            $questionText = $data->l4aa_text;
+        }
+        ?>
+            <li>{{$questionText}}</li>
+        <?php $k++; ?>
+        @endforeach
+    </ol>
+    <div class="text-center">
+        <button class="btn-primary btn btn-go" title="Go">Go! </button>
+    </div>
+</div>
+@else
+    No data found
+@endif
