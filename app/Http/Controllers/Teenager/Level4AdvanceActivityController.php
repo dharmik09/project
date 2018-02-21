e20524974171142c71c2ddbb62ded93ff8262110
Mansi <mansi.kanani@inexture.in>
2018-02-21 19:33:07 +0530

    l4 advance activity changes
    Signed-off-by: Mansi <mansi.kanani@inexture.in>

--- app/Http/Controllers/Teenager/Level4AdvanceActivityController.php
+++ app/Http/Controllers/Teenager/Level4AdvanceActivityController.php
@@ -76,63 +76,40 @@ class Level4AdvanceActivityController extends Controller {
         $professionId = Input::get('professionId');
         $typeId = Input::get('type');
         $total = $this->teenagersRepository->getTeenagerTotalBoosterPoints($userId);
-        if ($total > $this->level4MinimumPointsRequirements) {
-            $getTeenagerAttemptedProfession = $this->professionsRepository->getTeenagerAttemptedProfession($userId);
-            if (isset($getTeenagerAttemptedProfession) && !empty($getTeenagerAttemptedProfession)) {
-                foreach ($getTeenagerAttemptedProfession as $keyId) {
-                    $professionList[] = $keyId->id;
-                }
-            } else {
-                $professionList = array();
-            }
-            if (in_array($professionId, $professionList)) {
-                $professionId = intval($professionId);
-                $totalBasicQuestion = $this->level4ActivitiesRepository->getNoOfTotalQuestionsAttemptedQuestion($userId, $professionId);
-                $totalBasicQuestion[0]->NoOfAttemptedQuestions = 6;
-                if ($totalBasicQuestion[0]->NoOfTotalQuestions == 0) {
-                    $response['status'] = 0;
-                    $response['message'] = "Profession Doesn't have any basic questions"; 
-                    return response()->json($response, 200);
-                    exit;
-                } else if ($totalBasicQuestion[0]->NoOfTotalQuestions > $totalBasicQuestion[0]->NoOfAttemptedQuestions) {
-                    $response['status'] = 0;
-                    $response['message'] = "Play Basic to get to play Intermediate."; 
-                    return response()->json($response, 200);
-                    exit;
-                } else {
-                    $validTypeArr = array(Config::get('constant.ADVANCE_IMAGE_TYPE'), Config::get('constant.ADVANCE_DOCUMENT_TYPE'), Config::get('constant.ADVANCE_VIDEO_TYPE'));
-                    $typeId = intval($typeId);
-                    if (in_array($typeId, $validTypeArr)) {
-                        $typeId = $typeId;
-                    } else {
-                        $typeId = Config::get('constant.ADVANCE_IMAGE_TYPE');
-                    }
-                    //Get User activity
-                    $professionDetail = $this->professionsRepository->getProfessionsDataFromId($professionId);
-                    $level4AdvanceThumbImageUploadPath = $this->level4AdvanceThumbImageUploadPath;
-                    $level4AdvanceOriginalImageUploadPath = $this->level4AdvanceOriginalImageUploadPath;
-                    $userLevel4AdvanceImageTask = $this->level4ActivitiesRepository->getLevel4AdvanceActivityTaskByUser(Auth::guard('teenager')->user()->id, $professionId, Config::get('constant.ADVANCE_IMAGE_TYPE'));
-                    $userLevel4AdvanceDocumentTask = $this->level4ActivitiesRepository->getLevel4AdvanceActivityTaskByUser(Auth::guard('teenager')->user()->id, $professionId, Config::get('constant.ADVANCE_DOCUMENT_TYPE'));
-                    $userLevel4AdvanceVideoTask = $this->level4ActivitiesRepository->getLevel4AdvanceActivityTaskByUser(Auth::guard('teenager')->user()->id, $professionId, Config::get('constant.ADVANCE_VIDEO_TYPE'));
-                    //$level4Booster = Helpers::level4Booster($professionId, $userid);
-                    $level4Booster['total'] = $total;
-                    $response['level4Booster'] = $level4Booster;
-                    $response['boosterPoints'] = '';
-                    $response['status'] = 1;
-                    $response['message'] = trans('appmessages.default_success_msg');
-                    return view('teenager.basic.careerAdvanceQuizStep2Activity', compact('response', 'typeId', 'professionId', 'professionDetail', 'userLevel4AdvanceImageTask', 'level4AdvanceThumbImageUploadPath', 'level4AdvanceOriginalImageUploadPath', 'userLevel4AdvanceDocumentTask', 'userLevel4AdvanceVideoTask'));
-                    exit;
-                }
-            } else {
-                $response['status'] = 0;
-                $response['message'] = "Please, attempt profession first"; 
-                return response()->json($response, 200);
-                exit;
-            }
-        } else {
+        $professionId = intval($professionId);
+        $totalBasicQuestion = $this->level4ActivitiesRepository->getNoOfTotalQuestionsAttemptedQuestion($userId, $professionId);
+        if ($totalBasicQuestion[0]->NoOfTotalQuestions == 0) {
             $response['status'] = 0;
-            $response['message'] = trans('appmessages.not_sufficient_booster_points_msg');
+            $response['message'] = "Profession Doesn't have any basic questions"; 
             return response()->json($response, 200);
+            exit;
+        } else if ($totalBasicQuestion[0]->NoOfTotalQuestions > $totalBasicQuestion[0]->NoOfAttemptedQuestions) {
+            $response['status'] = 0;
+            $response['message'] = "Play Basic to get to play Intermediate."; 
+            return response()->json($response, 200);
+            exit;
+        } else {
+            $validTypeArr = array(Config::get('constant.ADVANCE_IMAGE_TYPE'), Config::get('constant.ADVANCE_DOCUMENT_TYPE'), Config::get('constant.ADVANCE_VIDEO_TYPE'));
+            $typeId = intval($typeId);
+            if (in_array($typeId, $validTypeArr)) {
+                $typeId = $typeId;
+            } else {
+                $typeId = Config::get('constant.ADVANCE_IMAGE_TYPE');
+            }
+            //Get User activity
+            $professionDetail = $this->professionsRepository->getProfessionsDataFromId($professionId);
+            $level4AdvanceThumbImageUploadPath = $this->level4AdvanceThumbImageUploadPath;
+            $level4AdvanceOriginalImageUploadPath = $this->level4AdvanceOriginalImageUploadPath;
+            $userLevel4AdvanceImageTask = $this->level4ActivitiesRepository->getLevel4AdvanceActivityTaskByUser(Auth::guard('teenager')->user()->id, $professionId, Config::get('constant.ADVANCE_IMAGE_TYPE'));
+            $userLevel4AdvanceDocumentTask = $this->level4ActivitiesRepository->getLevel4AdvanceActivityTaskByUser(Auth::guard('teenager')->user()->id, $professionId, Config::get('constant.ADVANCE_DOCUMENT_TYPE'));
+            $userLevel4AdvanceVideoTask = $this->level4ActivitiesRepository->getLevel4AdvanceActivityTaskByUser(Auth::guard('teenager')->user()->id, $professionId, Config::get('constant.ADVANCE_VIDEO_TYPE'));
+            //$level4Booster = Helpers::level4Booster($professionId, $userid);
+            $level4Booster['total'] = $total;
+            $response['level4Booster'] = $level4Booster;
+            $response['boosterPoints'] = '';
+            $response['status'] = 1;
+            $response['message'] = trans('appmessages.default_success_msg');
+            return view('teenager.basic.careerAdvanceQuizStep2Activity', compact('response', 'typeId', 'professionId', 'professionDetail', 'userLevel4AdvanceImageTask', 'level4AdvanceThumbImageUploadPath', 'level4AdvanceOriginalImageUploadPath', 'userLevel4AdvanceDocumentTask', 'userLevel4AdvanceVideoTask'));
             exit;
         }
     }
