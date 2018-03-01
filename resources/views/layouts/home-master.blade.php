d7b927689e0a99bd1644afb066a72de0802da715
inxronak <ronak.luhar@inexture.in>
2018-03-01 17:53:10 +0530

    google analytics code

--- resources/views/layouts/home-master.blade.php
+++ resources/views/layouts/home-master.blade.php
@@ -15,6 +15,15 @@
         <link href="{{asset('css/aos.css')}}" rel="stylesheet">
         <link href="{{asset('css/style.css')}}" rel="stylesheet">
         @stack('script-header')
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-85626973-1', 'auto');
+            ga('send', 'pageview');
+        </script>
         @yield('header')
     </head>
     <body class="fixed-nav {{ (Route::getFacadeRoot()->current()->uri() == 'teenager/signup') ? 'sec-overflow' : '' }}">
