4316194e8d6103439a021671f66359f7ad2173f0
Mansi <mansi.kanani@inexture.in>
2018-03-14 16:41:56 +0530

    Parent Panel - login page
    Signed-off-by: Mansi <mansi.kanani@inexture.in>

--- resources/views/parent/login.blade.php
+++ resources/views/parent/login.blade.php
@@ -49,10 +49,11 @@
                                 $email = '';
                             }
                         ?>
-                        <input type="text" class="form-control" id="email" maxlength="50" minlength="5" name="email" placeholder="{{trans('labels.emaillbl')}}" value="{{$email}}" autocomplete="off">
+                        <input type="text" class="form-control" id="email" maxlength="50" minlength="5" name="email" placeholder="email" value="{{$email}}" autocomplete="off">
+                        <span class="invalid" id="email_mobile_invalid" style="display: none;">Valid email required</span>
                     </div>
                     <div class="form-group">
-                        <input type="password" class="form-control pass-visi" id="password" maxlength="20" minlength="6" name="password" placeholder="{{trans('labels.passwordlbl')}}" autocomplete="off" tabindex="2">
+                        <input type="password" class="form-control pass-visi" id="password" maxlength="20" minlength="6" name="password" placeholder="password" autocomplete="off" tabindex="2">
                         <span class="visibility-pwd">
                             <img src="{{ Storage::url('img/view.png') }}" alt="view" class="view img">
                             <img src="{{ Storage::url('img/hide.png') }}" alt="view" class="img-hide hide img">
@@ -80,10 +81,6 @@
 <script type="text/javascript">
     jQuery(document).ready(function() {
         var loginRules = {
-            email: {
-                required: true,
-                email: true
-            },
             password: {
                 required: true,
                 minlength: 6,
@@ -93,16 +90,65 @@
         $("#login_form").validate({
             rules: loginRules,
             messages: {
-                email: {required: '<?php echo trans('validation.emailrequired') ?>'
-                },
                 password: {required: '<?php echo trans('validation.passwordrequired') ?>',
                     maxlength: 'Password maximum range is 20',
                     minlength: 'Password length is minimum 6'
                 }
             }
         });
-        
+        // Cache the toggle button
+        var $toggle = $(".visibility-pwd");
+        var $field = $(".pass-visi");
+         var i = $(this).find('.img');
+        // Toggle the field type
+        $toggle.on("click", function(e) {
+            e && e.preventDefault();
+            if ($field.attr("type") == "password") {
+                $field.attr("type", "text");
+                i.toggleClass("hide");
+            } else {
+               i.toggleClass("hide");
+                $field.attr("type", "password");
+            }
+
+        });
+
+        $("#login_form").submit(function() {
+            $("#loginSubmit").toggleClass('sending').blur();
+            var form = $("#login_form");
+            form.validate();
+            var validEmailOrMobile = false;
+            $('#email_mobile_invalid').show();
+            var emailOrMobile = $.trim($("#email").val());
+            if (emailOrMobile.length > 0 && emailOrMobile.match(/[a-zA-Z]/i)) {
+                if (validateEmail(emailOrMobile)) {
+                    var validEmailOrMobile = true;
+                }
+            }
+            if ($.isNumeric(emailOrMobile) && emailOrMobile.length > 9) {
+                var validEmailOrMobile = true;
+            }
+            if (validEmailOrMobile) {
+                $('#email_mobile_invalid').hide();
+                if (form.valid()) {
+                    return true;
+                }
+                setTimeout(function () {
+                    $("#loginSubmit").removeClass('sending').blur();
+                }, 2500);
+                return true;
+            } else {
+                $('#email_mobile_invalid').show();
+                $("#loginSubmit").removeClass('sending').blur();
+                return false;
+            }
+        });
     });
+
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
 </script>
 
 @stop
