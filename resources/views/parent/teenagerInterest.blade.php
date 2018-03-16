94bcdc7a9dafea33dfce1ee82114143ee753c68d
Mansi <mansi.kanani@inexture.in>
2018-03-16 17:03:21 +0530

    school login

---
+++ resources/views/parent/teenagerInterest.blade.php
@@ -0,0 +1,31 @@
+@if(isset($teenagerInterest) && count($teenagerInterest) > 0)
+<div class="intrest_content content_prime close_load">
+    <div class="row flex-container">
+        @forelse($teenagerInterest as $interestKey => $interestValue)
+        <div class="col-md-3 col-sm-3 col-xs-6 flex-items">
+            <div class="my_chart">
+                <div class="progress-radial progress-{{$interestValue['score']}} progress-potential">
+                    
+                </div>
+                <h4>{{ $interestValue['name']}}</h4>
+            </div>
+        </div>
+        @empty
+            <div class="col-md-12 col-sm-12 col-xs-12 flex-items ">
+                <center>
+                    <h3>Build your profile to discover professions matching your interests!</h3>
+                </center>
+            </div>
+        @endforelse
+    </div>
+</div>
+<p>
+    <a href="javascript:void(0)" class="load_more intrest_load">
+        <span>Show More</span>
+    </a>
+</p>
+@else
+@endif
+                 
+                        
+                    
