047c0042683ab30494aaf1a51ba8a87ce618af6b
Jaimin Pilojpara <jaimin.inexture@gmail.com>
2018-03-22 12:47:05 +0530

    profession institute Image upload APIs updated for it

--- resources/views/admin/ListProfessionInstitutes.blade.php
+++ resources/views/admin/ListProfessionInstitutes.blade.php
@@ -15,6 +15,38 @@
     </h1>
 </section>
 
+
+<div class="modal modal-centered fade" id="addInstituePhotoModal" tabindex="-1" role="dialog" aria-labelledby="addInstituePhotoModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2 class="modal-title" id="addInstituePhotoModalHeader">{{trans('labels.lbladdphotos')}}</h2>
+            </div>
+            <div class="modal-body">
+                <form id="deleteRelationshipManager" class="form-horizontal" method="post" action="{{ url('admin/saveProfessionInstitutePhoto') }}" enctype="multipart/form-data">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <input type="hidden" id="institute_id" name="institute_id" value="0">
+                    <input type="hidden" id="oldimage" name="oldimage" value="">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="institute_photo" class="col-sm-4 control-label">{{trans('labels.lblelselectinstitutephoto')}}</label>
+                            <div class="col-sm-6">
+                                <input type="file" class="form-control-file" id="institute_photo" name="institute_photo">
+                            </div>
+                        </div>
+                    </div><!-- /.box-body -->
+                    <div class="box-footer">
+                        <div class="pull-right">
+                            <button type="submit" class="btn save-btn bg-purple">{{trans('labels.savebtn')}}</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">{{trans('labels.cancelbtn')}}</button>
+                        </div>
+                    </div><!-- /.box-footer -->
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
 <section class="content">
     <div class="row">
          <div class="col-md-12">
@@ -36,6 +68,8 @@
                                 <th>{{trans('labels.lblmanagement')}}</th>
                                 <th>{{trans('labels.lblaccredationbody')}}</th>
                                 <th>{{trans('labels.lblaccredationscore')}}</th>
+                                <th>{{trans('labels.lblimage')}}</th>
+                                <th>{{trans('labels.lblaction')}}</th>
                             </tr>
                         </thead>
                      </table>
@@ -52,6 +86,14 @@
         var ajaxParams = {};
         getProfessionList(ajaxParams);
     });
+
+
+    function showProfessionImageUploadModel(id,schoolId,Image){
+        $("#institute_id").val(id);
+        $("#oldimage").val(Image);
+        $("#addInstituePhotoModalHeader").html("Update Photo for School Id : " + schoolId);
+        $('#addInstituePhotoModal').modal('show');
+    }
 
     var getProfessionList = function(ajaxParams){
         $('#listProfessionschoollist').DataTable({
@@ -81,7 +123,9 @@
                 { "data" : "affiliat_university" },
                 { "data" : "management" },
                 { "data" : "accreditation_body" },
-                { "data" : "accreditation_score" }
+                { "data" : "accreditation_score" },
+                { "data" : "image" },
+                { "data" : "action" }
             ]
         });
     };
