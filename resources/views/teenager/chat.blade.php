d43ecdebe604274cc78f83a1b9f761c240f380fb
Jaimin Pilojpara <jaimin.inexture@gmail.com>
2018-03-26 13:54:16 +0530

    chat page scroll to top

--- resources/views/teenager/chat.blade.php
+++ resources/views/teenager/chat.blade.php
@@ -447,7 +447,8 @@ function normalChat() {
     
     fetchNotification();
     $(document).ready(function(){
-        $(this).scrollTop(0);
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     });
     
 </script>
