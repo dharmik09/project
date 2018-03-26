3ac4ce156eb9ca88e00a35d1e647d8ba5158421e
Jaimin Pilojpara <jaimin.inexture@gmail.com>
2018-03-26 14:03:14 +0530

    chat page scroll issue

--- resources/views/teenager/chat.blade.php
+++ resources/views/teenager/chat.blade.php
@@ -446,7 +446,8 @@ function normalChat() {
     }
     
     fetchNotification();
-    $(document).ready(function(){
+
+    $(window).bind("load", function() {
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     });
