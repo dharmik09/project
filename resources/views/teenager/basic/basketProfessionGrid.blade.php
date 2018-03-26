8fb081d1865cdf708225bd0ed5d742088a70b924
inxronak <ronak.luhar@inexture.in>
2018-03-26 14:40:04 +0530

    change career grid view with images

--- resources/views/teenager/basic/basketProfessionGrid.blade.php
+++ resources/views/teenager/basic/basketProfessionGrid.blade.php
@@ -27,46 +27,24 @@
             </div>
         </div>
     </div>
-    <div class="career-map">
+    <div class="category-list career-listing">
         <div class="row">
             @if(isset($basketsData->profession) && count($basketsData->profession) > 0)
-                @foreach($basketsData->profession as $k => $v)
-                    <?php
-                        $average_per_year_salary = $v->professionHeaders->filter(function($item) {
-                                return $item->pfic_title == 'average_per_year_salary';
-                            })->first();
-                        $profession_outlook = $v->professionHeaders->filter(function($item) {
-                                return $item->pfic_title == 'profession_outlook';
-                            })->first();
-                    ?>
+                @foreach($basketsData->profession as $k => $v)           
                     <div class="col-md-4 col-sm-6">
-                        <?php $matchScale = ( isset($v->match_scale) && $v->match_scale != '') ? $v->match_scale : "career-data-nomatch"; ?>
-                        
-                        <div class="category {{$matchScale}}">
-                            <a href="{{url('teenager/career-detail')}}/{{$v->pf_slug}}" title="{{ $v->pf_name }}">{{ $v->pf_name }}</a>
-                            @if(isset($v->attempted) && $v->attempted == 1)
-                                <span class="complete">
-                                    <a href="#" title="Completed"><i class="icon-thumb"></i></a>
-                                </span>
-                            @endif
-                            <div class="overlay">
-                                @if(isset($average_per_year_salary))
-                                    <span class="salary">Average Salary per year : {!! ($countryId == 1) ? "<i class='fa fa-inr'></i>" : "<i class='fa fa-dollar'></i>" !!}
-                                        {{ (isset($average_per_year_salary->pfic_content) && !empty($average_per_year_salary->pfic_content)) ? strip_tags($average_per_year_salary->pfic_content) : '' }}
+                        <?php $matchScale = ( isset($v->match_scale) && $v->match_scale != '') ? $v->match_scale : "career-data-nomatch"; ?>            
+                        <div class="category-block {{$matchScale}}">
+                            <figure>
+                                <div class="category-img" style="background-image: url('{{Storage::url(Config::get('constant.PROFESSION_THUMB_IMAGE_UPLOAD_PATH').$v->pf_logo)}}')"></div>
+                                <figcaption>
+                                    <a href="{{url('teenager/career-detail')}}/{{$v->pf_slug}}" title="{{$v->pf_name}}">{{$v->pf_name}}</a>
+                                </figcaption>
+                                @if(isset($v->attempted) && $v->attempted == 1)
+                                    <span class="complete">
+                                        <a href="#" title="Completed"><i class="icon-thumb"></i></a>
                                     </span>
-                                @else
-                                    <span class="salary">Average Salary per year : N/A</span>
-                                @endif
-                            
-                                @if(isset($profession_outlook))
-                                    <span class="assessment">Outlook : 
-                                        {{ (isset($profession_outlook->pfic_content) && !empty($profession_outlook->pfic_content)) ? strip_tags(str_limit($profession_outlook->pfic_content,$limit=8,$end='')) : '' }}
-                                    </span>
-                                @else
-                                    <span class="assessment">Outlook : N/A</span>
-                                @endif
-                            </div>
-
+                                @endif                                         
+                            </figure>
                         </div>
                     </div>
                 @endforeach
