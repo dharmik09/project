369f4e2468046290fd547b41ffbe6a1317296be8
Mansi <mansi.kanani@inexture.in>
2018-03-27 16:30:13 +0530

    Teen panel - careers to consider functionality changes
    Signed-off-by: Mansi <mansi.kanani@inexture.in>

--- app/Http/Controllers/Teenager/ProfessionController.php
+++ app/Http/Controllers/Teenager/ProfessionController.php
@@ -775,25 +775,10 @@ class ProfessionController extends Controller {
         $user = Auth::guard('teenager')->user();
 
         if($user->id > 0) {
-            //Check career consider section coins consumption details
-            $componentsCareerConsider = $this->objPaidComponent->getPaidComponentsData(Config::get('constant.CAREER_TO_CONSIDER'));
-            $deductedCoinsCareerConsider = $this->objDeductedCoins->getDeductedCoinsDetailByIdForLS($user->id, $componentsCareerConsider->id, 1);
-            $remainingDaysForCareerConsider = 0;
-            if (!empty($deductedCoinsCareerConsider[0])) {
-                $remainingDaysForCareerConsider = Helpers::calculateRemainingDays($deductedCoinsCareerConsider[0]->dc_end_date);
-            }
-
             $teenagerCareers = $this->professionsRepository->getMyCareers($user->id);
             $getAllActiveProfessions = Helpers::getActiveProfessions();
             $getTeenagerHML = Helpers::getTeenagerMatchScale($user->id);
             
-            if ($remainingDaysForCareerConsider == 0) {
-                $response['status'] = 0;
-                $response['message'] = "Please consume your procoins to view your career suggestions!";
-                return response()->json($response, 200);
-                exit;
-            }
-
             if(!$getTeenagerHML) {
                 $response['status'] = 0;
                 $response['message'] = "Build your profile to know careers to consider!";
@@ -806,6 +791,20 @@ class ProfessionController extends Controller {
             $match = $nomatch = $moderate = $matchHigh = $matchLow = $moderateHigh = $moderateLow = $nomatchHign = $nomatchLow = $matchSecondHigh = $matchSecondLow = $moderateSecondHigh = $moderateSecondLow = [];
 
             if($getAllActiveProfessions) {
+                //Check career consider section coins consumption details
+                $componentsCareerConsider = $this->objPaidComponent->getPaidComponentsData(Config::get('constant.CAREER_TO_CONSIDER'));
+                $deductedCoinsCareerConsider = $this->objDeductedCoins->getDeductedCoinsDetailByIdForLS($user->id, $componentsCareerConsider->id, 1);
+                $remainingDaysForCareerConsider = 0;
+                if (!empty($deductedCoinsCareerConsider[0])) {
+                    $remainingDaysForCareerConsider = Helpers::calculateRemainingDays($deductedCoinsCareerConsider[0]->dc_end_date);
+                }
+                // if ($remainingDaysForCareerConsider == 0) {
+                //     $response['status'] = 0;
+                //     $response['message'] = "Please consume your procoins to view your career suggestions!";
+                //     return response()->json($response, 200);
+                //     exit;
+                // }
+
                 foreach($getAllActiveProfessions as $profession) {
                     $array = [];
                     $array['id'] = $profession->id;
@@ -934,7 +933,7 @@ class ProfessionController extends Controller {
                     $careerConsideration = $notSetArray;
                 }
                 
-                return view('teenager.basic.careerConsideration', compact('careerConsideration', 'getTeenagerHML'));
+                return view('teenager.basic.careerConsideration', compact('careerConsideration', 'getTeenagerHML', 'remainingDaysForCareerConsider', 'componentsCareerConsider'));
             } else {
                 $response['status'] = 0;
                 $response['message'] = "No professions found!";
