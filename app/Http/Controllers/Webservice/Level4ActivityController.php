a29b76916b1c9abc32861d812559e48b882f5ea3
Mansi <mansi.kanani@inexture.in>
2018-04-02 10:27:11 +0530

    Scholarship packages section active/inactive condition in getScholarshipProgramsDetails API
    Signed-off-by: Mansi <mansi.kanani@inexture.in>

--- app/Http/Controllers/Webservice/Level4ActivityController.php
+++ app/Http/Controllers/Webservice/Level4ActivityController.php
@@ -75,80 +75,86 @@ class Level4ActivityController extends Controller {
     }
 
     /* Request Params : getScholarshipProgramsDetails
-     *  loginToken, userId
+     *  loginToken, userId, careerId
      */
     public function getScholarshipProgramsDetails(Request $request) 
     {
         $response = [ 'status' => 0, 'login' => 0, 'message' => trans('appmessages.default_error_msg') ] ;
         $teenager = $this->teenagersRepository->getTeenagerById($request->userId);
         if ($teenager) {
-            //Get tenager sponsor's list
-            $teenagerSponsors = $this->teenagersRepository->getTeenagerSelectedSponsor($request->userId);
-            $sponsorArr = [];
-            if (isset($teenagerSponsors) && count($teenagerSponsors) > 0) {
-                foreach ($teenagerSponsors as $key => $val) {
-                    $sponsorArr[] = $val->ts_sponsor;
-                }
-            }
-            //Get scholarship programs list
-            $scholarshipPrograms = [];
-            if (!empty($sponsorArr)) {
-                if (!empty($sponsorArr)) {
-                    $scholarshipPrograms = $this->objSponsorsActivity->getActivityByTypeAndSponsor($sponsorArr, 3);
-                }
-            }
-            //Get list for applied scholarship programs
-            $appliedScholarshipDetails = $this->objTeenagerScholarshipProgram->getAllScholarshipProgramsByTeenId($request->userId);
-            $scholarshipProgramIds = [];
-            if (isset($appliedScholarshipDetails) && count($appliedScholarshipDetails) > 0) {
-                foreach ($appliedScholarshipDetails as $appliedScholarshipDetail) {
-                    $scholarshipProgramIds[] = $appliedScholarshipDetail->tsp_activity_id;
-                }
-            }
-            //Get list for expired scholarship programs
-            $expiredScholarshipPrograms = $this->objSponsorsActivity->getExpiredActivityByTypeAndSponsor($sponsorArr, 3);
-            $expiredActivityIds = [];
-            if (isset($expiredScholarshipPrograms) && count($expiredScholarshipPrograms) > 0) {
-                foreach ($expiredScholarshipPrograms as $expiredScholarshipProgram) {
-                    $expiredActivityIds[] = $expiredScholarshipProgram->id;
-                }
-            }
-            $exceptScholarshipIds = array_unique(array_merge($scholarshipProgramIds, $expiredActivityIds));
-            $scholarshipDetailsArr = [];
-            foreach ($scholarshipPrograms as $scholarshipProgram) {
-                $scholarshipData = [];
-                $scholarshipData['id'] = $scholarshipProgram->id;
-                if ($scholarshipProgram->sa_image != "" && Storage::size(Config::get('constant.SA_ORIGINAL_IMAGE_UPLOAD_PATH') . $scholarshipProgram->sa_image) > 0) {
-                    $scholarshipData['image'] = Storage::url(Config::get('constant.SA_THUMB_IMAGE_UPLOAD_PATH') . $scholarshipProgram->sa_image);
-                } else {
-                    $scholarshipData['image'] = Storage::url(Config::get('constant.SA_THUMB_IMAGE_UPLOAD_PATH') . 'proteen-logo.png');
-                }
-                $scholarshipData['companyName'] = $scholarshipProgram->sp_company_name;
-                $scholarshipData['title'] = $scholarshipProgram->sa_name;
-                $scholarshipData['details'] = $scholarshipProgram->sa_description;
-                $scholarshipData['learnMoreLink'] = url('teenager/learnMoreL4');
-                $scholarshipData['is_expired'] = (!empty($expiredActivityIds) && in_array($scholarshipProgram->id, $expiredActivityIds)) ? 1 : 0;
-                $scholarshipData['is_applied'] = (!empty($scholarshipProgramIds) && in_array($scholarshipProgram->id, $scholarshipProgramIds)) ? 1 : 0;
-                if (!empty($expiredActivityIds) && in_array($scholarshipProgram->id, $expiredActivityIds)) {
-                    $scholarshipData['activityStatus'] = "expired";
-                } else {
-                    if (!empty($appliedScholarshipDetails) && in_array($scholarshipProgram->id, $scholarshipProgramIds)) {
-                        $scholarshipData['activityStatus'] = "applied";
-                    } else {
-                        $scholarshipData['activityStatus'] = "apply";
+            if (isset($request->careerId) && $request->careerId != '') {  
+                //Get tenager sponsor's list
+                $teenagerSponsors = $this->teenagersRepository->getTeenagerSelectedSponsor($request->userId);
+                $sponsorArr = [];
+                if (isset($teenagerSponsors) && count($teenagerSponsors) > 0) {
+                    foreach ($teenagerSponsors as $key => $val) {
+                        $sponsorArr[] = $val->ts_sponsor;
                     }
                 }
-                $scholarshipDetailsArr[] = $scholarshipData;
+                //Get scholarship programs list
+                $scholarshipPrograms = [];
+                if (!empty($sponsorArr)) {
+                    if (!empty($sponsorArr)) {
+                        $scholarshipPrograms = $this->objSponsorsActivity->getActivityByTypeAndSponsor($sponsorArr, 3);
+                    }
+                }
+                //Get list for applied scholarship programs
+                $appliedScholarshipDetails = $this->objTeenagerScholarshipProgram->getAllScholarshipProgramsByTeenId($request->userId);
+                $scholarshipProgramIds = [];
+                if (isset($appliedScholarshipDetails) && count($appliedScholarshipDetails) > 0) {
+                    foreach ($appliedScholarshipDetails as $appliedScholarshipDetail) {
+                        $scholarshipProgramIds[] = $appliedScholarshipDetail->tsp_activity_id;
+                    }
+                }
+                //Get list for expired scholarship programs
+                $expiredScholarshipPrograms = $this->objSponsorsActivity->getExpiredActivityByTypeAndSponsor($sponsorArr, 3);
+                $expiredActivityIds = [];
+                if (isset($expiredScholarshipPrograms) && count($expiredScholarshipPrograms) > 0) {
+                    foreach ($expiredScholarshipPrograms as $expiredScholarshipProgram) {
+                        $expiredActivityIds[] = $expiredScholarshipProgram->id;
+                    }
+                }
+                $exceptScholarshipIds = array_unique(array_merge($scholarshipProgramIds, $expiredActivityIds));
+                $scholarshipDetailsArr = [];
+                foreach ($scholarshipPrograms as $scholarshipProgram) {
+                    $scholarshipData = [];
+                    $scholarshipData['id'] = $scholarshipProgram->id;
+                    if ($scholarshipProgram->sa_image != "" && Storage::size(Config::get('constant.SA_ORIGINAL_IMAGE_UPLOAD_PATH') . $scholarshipProgram->sa_image) > 0) {
+                        $scholarshipData['image'] = Storage::url(Config::get('constant.SA_THUMB_IMAGE_UPLOAD_PATH') . $scholarshipProgram->sa_image);
+                    } else {
+                        $scholarshipData['image'] = Storage::url(Config::get('constant.SA_THUMB_IMAGE_UPLOAD_PATH') . 'proteen-logo.png');
+                    }
+                    $scholarshipData['companyName'] = $scholarshipProgram->sp_company_name;
+                    $scholarshipData['title'] = $scholarshipProgram->sa_name;
+                    $scholarshipData['details'] = $scholarshipProgram->sa_description;
+                    $scholarshipData['learnMoreLink'] = url('teenager/learnMoreL4');
+                    $scholarshipData['is_expired'] = (!empty($expiredActivityIds) && in_array($scholarshipProgram->id, $expiredActivityIds)) ? 1 : 0;
+                    $scholarshipData['is_applied'] = (!empty($scholarshipProgramIds) && in_array($scholarshipProgram->id, $scholarshipProgramIds)) ? 1 : 0;
+                    if (!empty($expiredActivityIds) && in_array($scholarshipProgram->id, $expiredActivityIds)) {
+                        $scholarshipData['activityStatus'] = "expired";
+                    } else {
+                        if (!empty($appliedScholarshipDetails) && in_array($scholarshipProgram->id, $scholarshipProgramIds)) {
+                            $scholarshipData['activityStatus'] = "applied";
+                        } else {
+                            $scholarshipData['activityStatus'] = "apply";
+                        }
+                    }
+                    $scholarshipDetailsArr[] = $scholarshipData;
+                }
+                $data['information'] = "Instructions: Here are tasks by your real world sponsors. Impress them & build your professional reputation early on!";
+                $professionCompletePercentage = Helpers::getProfessionCompletePercentage($request->userId, $request->careerId);
+                $data['is_active'] = (isset($professionCompletePercentage) && $professionCompletePercentage >= 100) ? 1 : 0;
+                $data['scholarshipPrograms'] = $scholarshipDetailsArr;
+                $response['status'] = 1;
+                $response['message'] = trans('appmessages.default_success_msg');
+                $response['data'] = $data;
+                //Store log in System
+                $this->log->info('Retrieve scholarship programs details', array('userid'=>$request->userId));
+            } else {
+                $response['status'] = 0;
+                $response['message'] = $response['message'] = trans('appmessages.missing_data_msg');;
             }
-            //Store log in System
-            $this->log->info('Retrieve scholarship programs details', array('userid'=>$request->userId));
-            $data['information'] = "Instructions: Here are tasks by your real world sponsors. Impress them & build your professional reputation early on!";
-            $data['is_active'] = rand(0, 1);
-            $data['scholarshipPrograms'] = $scholarshipDetailsArr;
             $response['login'] = 1;
-            $response['status'] = 1;
-            $response['message'] = trans('appmessages.default_success_msg');
-            $response['data'] = $data;
         } else {
             $response['message'] = trans('appmessages.invalid_userid_msg') . ' or ' . trans('appmessages.notvarified_user_msg');
         }
