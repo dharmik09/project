d13533e81a521b2816e4fa8fdf65747349b36405
Mansi <mansi.kanani@inexture.in>
2018-05-03 19:23:17 +0530

    send connection request issue resolved
    Signed-off-by: Mansi <mansi.kanani@inexture.in>

--- app/Services/Community/Repositories/EloquentCommunityRepository.php
+++ app/Services/Community/Repositories/EloquentCommunityRepository.php
@@ -236,7 +236,14 @@ class EloquentCommunityRepository extends EloquentBaseRepository implements Comm
                 $updateRequest = $this->model->where('id', $availableRequest->id)->update($connectionRequestDetails);
                 $return = $this->model->where('id', $availableRequest->id)->first();
             } else {
-                $return = $this->model->create($connectionRequestData);
+                $reverseRequest = $this->model->where('tc_receiver_id', $connectionRequestData['tc_sender_id'])->where('tc_sender_id', $connectionRequestData['tc_receiver_id'])->first();
+                if (isset($reverseRequest) && !empty($reverseRequest)) {
+                    $connectionRequestDetails['tc_status'] = Config::get('constant.CONNECTION_PENDING_STATUS');
+                    $updateRequest = $this->model->where('id', $reverseRequest->id)->update($connectionRequestDetails);
+                    $return = $this->model->where('id', $reverseRequest->id)->first();
+                } else {
+                    $return = $this->model->create($connectionRequestData);
+                }
             }
         }
         return $return;
