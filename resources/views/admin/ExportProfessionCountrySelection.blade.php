5db23767b09e19a902b92abb44e87c115dff7f2c
inxronak <ronak.luhar@inexture.in>
2018-05-04 20:05:10 +0530

    view file for profession export country wise

---
+++ resources/views/admin/ExportProfessionCountrySelection.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.admin-master')
+
+@section('content')
+
+<!-- Content Wrapper. Contains page content -->
+
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        Export {{trans('labels.professions')}} Data
+    </h1>
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <div class="row">
+        <!-- right column -->
+        <div class="col-md-12">
+            <!-- Horizontal Form -->
+            <div class="box box-info">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Select country for which you want to export profession data</h3>
+                </div><!-- /.box-header -->
+                @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <strong>{{trans('validation.whoops')}}</strong>{{trans('validation.someproblems')}}<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+
+                <form id="addProfessionBulk" class="form-horizontal" method="post" files="true" action="{{ url('/admin/exportProfessoin') }}" enctype="multipart/form-data">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="p_country" class="col-sm-2 control-label">Country</label>
+                            <div class="col-sm-4">
+                                <select class="form-control" id="p_country" name="p_country">
+                                  <option value="">{{trans('labels.formlblselectcountry')}}</option>
+                                    @forelse($countryList as $key => $value)
+                                        <option value="{{$value->id}}">{{$value->c_name}}</option>
+                                    @empty
+                                    @endforelse
+                                </select>
+                            </div>
+                        </div>                  
+                    </div>
+                    <div class="box-footer">
+                        <button type="submit" id="submit" class="btn btn-primary btn-flat" >Export</button>
+                        <a class="btn btn-danger btn-flat pull-right" href="{{ url('admin/professions') }}">{{trans('labels.cancelbtn')}}</a>
+                    </div><!-- /.box-footer -->
+                </form>
+            </div>   <!-- /.row -->
+        </div>
+    </div>
+</section><!-- /.content -->
+
+@stop
+
+@section('script')
+<script type="text/javascript">
+    jQuery(document).ready(function() {
+
+            var validationRules = {
+                 p_bulk : {
+                    required : true
+                }
+            }
+
+
+        $("#addProfessionBulk").validate({
+            rules : validationRules,
+            messages : {
+                  p_bulk : {
+                    required : "<?php echo trans('validation.requiredfield'); ?>"
+                }
+            }
+        })
+    });
+
+
+</script>
+@stop
+
