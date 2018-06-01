b1a0873d94cadbedb7278b8c3fb8a1ffd3cbc7d4
inxronak <ronak.luhar@inexture.in>
2018-06-01 15:28:32 +0530

    upload new youtube video link in teenager landing page

--- resources/views/teenager/index.blade.php
+++ resources/views/teenager/index.blade.php
@@ -7,8 +7,9 @@
 @section('content')
     <div class="banner-landing">
         <div class="container">
+            <div class="play-icon"><a href="javascript:void(0);" class="play-btn" id="iframe-video-click"><img src="img/play-icon.png" alt="play icon"></a></div>
         </div>
-        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/WoelVRjFO4A?autohide=1&amp;showinfo=0&amp;modestBranding=1&amp;start=0&amp;rel=0&amp;enablejsapi=1" frameborder="0" allowfullscreen id="iframe-video"></iframe>
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/hF3CYmBrfyQ?autohide=1&amp;showinfo=0&amp;modestBranding=1&amp;start=0&amp;rel=0&amp;enablejsapi=1" frameborder="0" allowfullscreen id="iframe-video"></iframe>
     </div>
     <!-- teen bio-->
     <section class="teen-bio">
@@ -151,6 +152,7 @@
                 }
             });
             $('#iframe-video-click').on('click', function(ev) {
+                $('iframe').show();
                 $("#iframe-video")[0].src += "&autoplay=1";
                 ev.preventDefault();
             });
